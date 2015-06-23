c63f6ea977ce8959dad161c679a505e1133bca16
Daehyun Kim <hatemogi@gmail.com>
2015-06-23 20:26:03 +0900

    prelude 쓰는 기준 기본 emacs 설정 추가

---
+++ cofcod.el
@@ -0,0 +1,11 @@
+(require 'prelude-clojure)
+(require 'prelude-emacs-lisp)
+(require 'prelude-lisp)
+(require 'prelude-ido)
+(require 'prelude-js)
+(require 'prelude-css)
+(require 'prelude-org)
+
+(menu-bar-mode -1)
+(setq whitespace-style '(whitespace-line-column nil))
+(server-start)
