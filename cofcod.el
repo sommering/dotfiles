488976fd927e6b1412cad8e4879e16efac5c4ce6
오후코드 <ohucode@gmail.com>
2015-10-28 21:31:12 +0900

    helm 설정 추가

--- cofcod.el
+++ cofcod.el
@@ -5,14 +5,31 @@
 (require 'prelude-js)
 (require 'prelude-css)
 (require 'prelude-org)
+(require 'prelude-helm-everywhere)
 
 (if (display-graphic-p)
   (set-fontset-font "fontset-default" 'hangul
                     "KoPubDotum_Pro Light"))
+
+(setq magit-last-seen-setup-instructions "1.4.0")
 
 (global-set-key [C-tab] 'next-buffer)
 (global-set-key [C-S-tab] 'previous-buffer)
 
 (menu-bar-mode -1)
 (setq whitespace-style '(whitespace-line-column nil))
+(global-company-mode)
+(projectile-global-mode)
 (server-start)
+
+(require 'clojure-mode)
+
+(define-clojure-indent
+  (defroutes 'defun)
+  (GET 2)
+  (POST 2)
+  (PUT 2)
+  (DELETE 2)
+  (HEAD 2)
+  (ANY 2)
+  (context 2))
